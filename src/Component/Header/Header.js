32fb2fab6f32afae571ebcbaf20377b4d12fe419
Md-Istiaq <mdistiaq102@outlook.com>
2022-04-18 01:11:20 +0600

    log out button implemented

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,8 +1,18 @@
+import { getAuth ,signOut} from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import app from '../../_firebase.init';
 import './Header.css'
 const Header = () => {
+
+  const auth = getAuth(app)
+
+  const [user] = useAuthState(auth)
+  const logout = () =>{
+      signOut(auth)
+  }
     return (
      <div className='header'>
       <Navbar bg="dark" variant="dark">
@@ -13,7 +23,12 @@ const Header = () => {
      <Link to="/checkout">Check Out</Link>
      <Link to="/blogs">Blogs</Link>
      <Link to="/aboutme">About Me</Link>
-     <Link to="/login">Log In</Link>
+     {
+       user?
+       <button className='button' onClick={logout}>Log Out</button>
+       :
+       <Link to="/login">Log In</Link>
+     }
     </Nav>
     </Container>
   </Navbar>
