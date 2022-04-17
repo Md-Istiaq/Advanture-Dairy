49826c08ba1e93e75d798efb18f33beb71d333db
Md-Istiaq <mdistiaq102@outlook.com>
2022-04-17 19:30:53 +0600

    header updated

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Blogs = () => {
     return (
         <div>
-              <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
         </div>
     );
 };
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
      <div className='header'>
-     <Link to="/home">Home</Link>
+      <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="/">Adventure dairy</Navbar.Brand>
+    <Nav className="me-auto">
+    <Link to="/home">Home</Link>
      <Link to="/checkout">Check Out</Link>
      <Link to="/blogs">Blogs</Link>
      <Link to="/aboutme">About Me</Link>
+    </Nav>
+    </Container>
+  </Navbar>
      </div>
     );
 };
